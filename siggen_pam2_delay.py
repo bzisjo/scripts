e1ecdfd818b76ff4461ef8c44a8097d7c0c33377
Jonathan Wang <bzisjo@gmail.com>
2018-10-07 18:51:07 -0700

    fixed script

--- siggen_pam2_delay.py
+++ siggen_pam2_delay.py
@@ -41,45 +41,70 @@ def format_neg(vec):
             return [(0.0, zy)] + vec[i:]
 
 
-def code_to_voltage(code, amp):
-    return (code - 1.5) / 1.5 * amp
+# def code_to_voltage(code, amp):
+#     return (code - 1.5) / 1.5 * amp
+def code_to_value(code, type):
+    if type == 'current':
+        return '\'ileak\'' if code == 0 else '\'ihold\''
+    elif type == 'voltage':
+        return 0 if code == 0 else '\'xvcc\''
 
-
-def get_time_value_pairs(pattern, tbit, amp, td=0, tr=10e-12):
-    vinit = code_to_voltage(pattern[0], amp)
+def get_time_value_pairs(pattern, tbit, tinterval, td=0, tr=10e-9):
+    td_s = 2e-9
+    iinit = 0
+    vinit = 0
     tcur = td
     if tcur >= 0.0:
-        outac = [(0.0, vinit)]
+        data_pwl = [(0.0, iinit)]
+        signal_pwl = [(0.0, vinit)]
     else:
-        outac = [(tcur, vinit)]
+        data_pwl = [(tcur - td_s, iinit)]
+        signal_pwl = [(tcur, vinit)]
 
     if tcur > 0.0:
-        outac.append((tcur, vinit))
+        print('{}'.format(tcur-td_s))
+        data_pwl.append((tcur - td_s, iinit))
+        signal_pwl.append((tcur, vinit))
         
     for idx, code in enumerate(pattern):
-        vac = code_to_voltage(code, amp)
-        vac_next = vac if idx == len(pattern) - 1 else code_to_voltage(pattern[idx + 1], amp)
-        outac.append((tcur + tbit - tr, vac))
-        outac.append((tcur + tbit, vac_next))
-        tcur += tbit
+        icur = code_to_value(code, 'current')
+        vhi = code_to_value(1, 'voltage')
+        vlo = code_to_value(0, 'voltage')
+        # vcur = code_to_value(code, 'voltage')
+        # inxt = icur if idx == len(pattern) - 1 else code_to_value(pattern[idx + 1], 'current')
+        # vnxt = vcur if idx == len(pattern) - 1 else code_to_value(pattern[idx + 1], 'voltage')
+        data_pwl.append((tcur - td_s + tr, icur))
+        data_pwl.append((tcur + td_s + tr + tbit, icur))
+        data_pwl.append((tcur + td_s + tr*2 + tbit, 0))
+        data_pwl.append((tcur - td_s + tr*2 + tbit + tinterval, 0))
+        signal_pwl.append((tcur + tr, vhi))
+        signal_pwl.append((tcur + tr + tbit, vhi))
+        signal_pwl.append((tcur + tr*2 + tbit, vlo))
+        signal_pwl.append((tcur + tr*2 + tbit + tinterval, vlo))
+        tcur += tr*2 + tbit + tinterval
 
-    return format_neg(outac)
+    return format_neg(data_pwl), format_neg(signal_pwl)
 
 if __name__ == '__main__':
-    nsym = 4
-    nlen = 5
+    nsym = 2
+    nlen = 3
     data = de_bruijn(nsym, nlen)
     data = data + data[:2 * nlen]
     print(data)
-    tbit = 120e-12
-    amp = 1.0
-    tdelay = 50e-12
-    tr = 20e-12
-    fname = './pam4_data_200p.txt'
+    tbit = 10e-9
+    tinterval = 55e-9
+    tdelay = 5e-9
+    tr = 300e-12
+    data_pwl = './data_pwl_{}_{}_{}_{}.txt'.format(nsym, nlen, tbit, tinterval)
+    switch_pwl = './switch_pwl_{}_{}_{}_{}.txt'.format(nsym, nlen, tbit, tinterval)
+    data_out, signal_out = get_time_value_pairs(data, tbit, tinterval, td=tdelay, tr=tr)
 
-    inac = get_time_value_pairs(data, tbit, amp, td=tdelay, tr=tr)
+    with open(data_pwl, 'w') as f:
+        for t, v in data_out:
+            f.write('{:.6g} {}\n'.format(t, v))
+        print('last time = %.4g' % t)
 
-    with open(fname, 'w') as f:
-        for t, v in inac:
-            f.write('{:.6g} {:.6g}\n'.format(t, v))
+    with open(switch_pwl, 'w') as f:
+        for t, v in signal_out:
+            f.write('{:.6g} {}\n'.format(t, v))
         print('last time = %.4g' % t)
